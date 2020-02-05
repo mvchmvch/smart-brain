c15bb678bf70a80d23ceaecce09687d91ab0d15e
Mikhail Chudakov <mvch@list.ru>
2020-02-05 18:26:49 +0300

    added multiple faces recognition

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl, box }) => {
-  return (
-    <div className='center ma'>
-      <div className='absolute mt2'>
-        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
-        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
-      </div>
-    </div>
-  );
+const FaceRecognition = ({ imageUrl, boxes }) => {
+
+	const boundingBoxes = boxes.map((box, index) => {
+  		return <div className='bounding-box' key={index}  style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+  	})
+    	
+  	return (
+    	<div className='center ma'>
+      		<div className='absolute mt2'>
+        		<img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
+        		{boxes.length >0 && boundingBoxes}
+      		</div>
+    	</div>
+  );	
+  
 }
 
 export default FaceRecognition;
